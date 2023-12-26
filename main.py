50fbc4110c1395c335d3d2fa32e3bef2902ed7fa
NN <asdzxc123fg@gmail.com>
2023-12-26 16:14:45 +0200

    Hm ex 1 done

--- main.py
+++ main.py
@@ -1,12 +1,9 @@
-a = int(input("a:"))
-b = int(input("b:"))
-count = 0
-sum = 0
-if a > b:
-    a, b = b, a
-for i in range(a+1,b):
-    print(i, end=' ')
-    sum += i
-    count += 1
-print(f"Total sum is {sum}")
-print(f"Average is {sum/count}")
+a=int(input("Первое число: "))
+
+b=int(input("Второе число: "))
+
+for i in range(a,b+1):
+
+  if i%7==0:
+
+      print(i)
