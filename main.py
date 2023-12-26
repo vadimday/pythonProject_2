89db6b758ebe1cbb24e56fe404593a8ca7b54df3
NN <asdzxc123fg@gmail.com>
2023-12-26 16:25:04 +0200

    Lab ex 3 done

--- main.py
+++ main.py
@@ -1,12 +1,4 @@
-a = int(input("a:"))
-b = int(input("b:"))
-count = 0
-sum = 0
-if a > b:
-    a, b = b, a
-for i in range(a+1,b):
-    print(i, end=' ')
-    sum += i
-    count += 1
-print(f"Total sum is {sum}")
-print(f"Average is {sum/count}")
+length = int(input("Линия: "))
+
+for i in range(length):
+    print('*', end="")
